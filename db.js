490f7eb6bb3b0fa58bd88f9e5647562b4c61b812
danielPanawan853 <danadaniel897@gmail.com>
2025-07-04 14:54:34 +0700

    update commit

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',       // Ganti kalau usermu beda
-  password: '',       // Ganti kalau pakai password
-  database: 'react_db' // Pastikan database ini SUDAH ADA
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
+
 
 db.connect((err) => {
   if (err) {
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -78,6 +78,5 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(5000, () => {
-  console.log('Server berjalan di http://localhost:5000');
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server berjalan di http://localhost:${PORT}`));
