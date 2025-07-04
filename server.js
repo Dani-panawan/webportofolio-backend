9a5ca5445d8ff501c8e3474315282717c3565882
danielPanawan853 <danadaniel897@gmail.com>
2025-07-04 16:52:47 +0700

    initial commit

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ import express from 'express';
 import mysql from 'mysql2';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.js';
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -78,5 +80,6 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server berjalan di http://localhost:${PORT}`));
+app.listen(5000, () => {
+  console.log('Server berjalan di http://localhost:5000');
+});
